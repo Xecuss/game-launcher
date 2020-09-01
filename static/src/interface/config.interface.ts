abfeddb5e561f854760c4ce8837821cf4cb11337
Xecuss <imkkai@hotmail.com>
2020-09-01 20:17:25 +0800

    添加多Game配置接口

--- static/src/interface/config.interface.ts
+++ static/src/interface/config.interface.ts
@@ -1,5 +1,3 @@
-import { ILocalNetwork } from "./localNet.interface";
-
 export interface ILauncherConfig{
     use720p: boolean;
     window: boolean;
@@ -8,6 +6,25 @@ export interface ILauncherConfig{
     printerFormat: 'png' | 'bmp' | 'tga' | 'jpg';
     printerClear: boolean;
     useAbleNetWorkConf: Array<INetworkConfig>;
+    nowUseNetwork: number;
+    nowLocalNetwork: string;
+    screenShotPath: string;
+    ea: boolean;
+    //spice api选项，打开后可以通过工具远程控制spice
+    api: boolean;
+    apiPassword: string;
+}
+
+export interface IGameConfig{
+    id: number;
+    name: string;
+    path: string;
+    use720p: boolean;
+    window: boolean;
+    usePrinter: boolean;
+    printerPath: string;
+    printerFormat: 'png' | 'bmp' | 'tga' | 'jpg';
+    printerClear: boolean;
     nowUseNetwork: number;
     nowLocalNetwork: string;
     screenShotPath: string;
