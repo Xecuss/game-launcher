b1044164148e3b11b9ae10188d8ceee4db33cf0b
Xecuss <imkkai@hotmail.com>
2020-09-22 19:55:20 +0800

    准备添加全局设置页，但是由于现在的设置项比较少所以暂时不加

---
+++ static/src/pages/globalSetting.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="page beautify-scroll">
+    <p class="block-title">spice</p>
+    <div class="mui-panel">
+        <div class="mui-checkbox">
+            <label>
+                <input type="checkbox" v-model="conf.enableMSCM"> 启用多Spice配置管理
+            </label>
+        </div>
+    </div>
+</div>
+</template>
